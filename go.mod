3d295c2b4a185fc39dcb799d6cc7d8457b7b4327
xietongqing <xietongqing@teddymobile.cn>
2023-09-07 00:36:58 +0800

    use redis implement mq to replace kafka

--- go.mod
+++ go.mod
@@ -3,27 +3,39 @@ module file-push
 go 1.19
 
 require (
-	github.com/fsnotify/fsnotify v1.6.0 // indirect
-	github.com/go-redis/redis v6.15.9+incompatible // indirect
+	github.com/fsnotify/fsnotify v1.6.0
+	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/jlaffaye/ftp v0.2.0
+	github.com/segmentio/kafka-go v0.4.42
+	github.com/spf13/viper v1.16.0
+	go.uber.org/zap v1.21.0
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1
+)
+
+require (
+	github.com/demdxx/gocast v1.2.0 // indirect
 	github.com/gomodule/redigo v1.8.9 // indirect
 	github.com/hashicorp/errwrap v1.0.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
-	github.com/jlaffaye/ftp v0.2.0 // indirect
 	github.com/klauspost/compress v1.15.9 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/onsi/ginkgo v1.16.5 // indirect
+	github.com/onsi/gomega v1.27.10 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
 	github.com/pierrec/lz4/v4 v4.1.15 // indirect
-	github.com/segmentio/kafka-go v0.4.42 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/spf13/afero v1.9.5 // indirect
 	github.com/spf13/cast v1.5.1 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/spf13/viper v1.16.0 // indirect
 	github.com/subosito/gotenv v1.4.2 // indirect
-	golang.org/x/sys v0.8.0 // indirect
-	golang.org/x/text v0.9.0 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.8.0 // indirect
+	golang.org/x/net v0.12.0 // indirect
+	golang.org/x/sys v0.10.0 // indirect
+	golang.org/x/text v0.11.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -4,21 +4,100 @@ import (
 	"context"
 	"encoding/json"
 	"file-push/global"
-	"file-push/limit"
+	"file-push/log"
 	"file-push/models"
 	mq "file-push/mq"
 	"file-push/redis"
+	redmq "file-push/redis/redismq"
 	"file-push/tool"
 	"fmt"
+	"io"
+	"net/http"
 	"time"
 )
 
 func main() {
 	global.GVA_VP = tool.Viper()
-	redis.InitRedis()
-	go generateMessage()
-	go mq.ListenerSignal()
-	mq.ConsumeMessageFromKafka(limit.New(2))
+
+	//redis.InitRedis()
+	//go generateMessage()
+	//go mq.ListenerSignal()
+	//mq.ConsumeMessageFromKafka(limit.New(2))
+	go startConsumer()
+	runServer()
+}
+func startConsumer() {
+	client := redis.NewClient("tcp", "127.0.0.1:6379", "")
+	// 接收到消息后的处理函数
+	callbackFunc := func(ctx context.Context, msg *redis.MsgEntity) error {
+		log.Infof("receive msg, msg id: %s, msg key: %s, msg val: %s", msg.MsgID, msg.Key, msg.Val)
+		return nil
+	}
+
+	// 自定义实现的死信队列
+	demoDeadLetterMailbox := NewDemoDeadLetterMailbox(func(msg *redis.MsgEntity) {
+		log.Infof("receive dead letter, msg id: %s, msg key: %s, msg val: %s", msg.MsgID, msg.Key, msg.Val)
+	})
+
+	// 构造并启动消费者
+	_, err := redmq.NewConsumer(client, "myTopic", "consumerGroup", "consumerID", callbackFunc,
+		// 每条消息最多重试 2 次
+		redmq.WithMaxRetryLimit(2),
+		// 每轮接收消息的超时时间为 2 s
+		redmq.WithReceiveTimeout(2*time.Second),
+		// 注入自定义实现的死信队列
+		redmq.WithDeadLetterMailbox(demoDeadLetterMailbox))
+	if err != nil {
+		log.Errorf("", err)
+		return
+	}
+}
+
+// 自定义实现的死信队列
+type DemoDeadLetterMailbox struct {
+	do func(msg *redis.MsgEntity)
+}
+
+func NewDemoDeadLetterMailbox(do func(msg *redis.MsgEntity)) *DemoDeadLetterMailbox {
+	return &DemoDeadLetterMailbox{
+		do: do,
+	}
+}
+
+// 死信队列接收消息的处理方法
+func (d *DemoDeadLetterMailbox) Deliver(ctx context.Context, msg *redis.MsgEntity) error {
+	d.do(msg)
+	return nil
+}
+
+func runServer() {
+	log.Infof("start server ...")
+	http.HandleFunc("/", pushFileByFtp)      // 设置访问的路由
+	err := http.ListenAndServe(":9090", nil) // 设置监听的端口
+	if err != nil {
+		log.Fatalf("ListenAndServe: ", err)
+	}
+}
+func pushFileByFtp(w http.ResponseWriter, r *http.Request) {
+	ftpParams, _ := io.ReadAll(r.Body)
+	ftpMessage := models.FtpMessage{}
+	if err := json.Unmarshal(ftpParams, &ftpMessage); err != nil {
+		log.Errorf("message is not a json string %s", string(ftpParams))
+		fmt.Fprintf(w, "message is not a json string")
+		return
+	}
+
+	client := redis.NewClient("tcp", "127.0.0.1:6379", "")
+	// 最多保留十条消息
+	producer := redmq.NewProducer(client, redmq.WithMsgQueueLen(10))
+	ctx := context.Background()
+	msgID, err := producer.SendMsg(ctx, "myTopic", "test_kk", string(ftpParams))
+	if err != nil {
+		log.Errorf("send to redis error ", err)
+		return
+	}
+	log.Infof(msgID)
+
 }
 func generateMessage() {
 	time.Sleep(10 * time.Second)
